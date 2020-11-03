56c206eda063388f58c12278bd2257dbfbb56f3b
Egami-yuk <e.y062097@gmail.com>
2020-11-03 20:20:31 +0900

    before_action再記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -22,10 +24,6 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
-      
   end
   
   def update
@@ -43,5 +41,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
