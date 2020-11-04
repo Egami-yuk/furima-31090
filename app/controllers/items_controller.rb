636e23d4a362098ccdb3f5f54453c639d9fd0368
Egami-yuk <e.y062097@gmail.com>
2020-11-04 11:21:49 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to root_path
     else
-      render :show
+      render :edit
     end
   end
 
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      redner :edit
+      redner :show
     end
   end
 
