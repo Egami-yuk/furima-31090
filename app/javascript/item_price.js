3614e9831f0b0c2b46c06354afdecea9f378e58f
Egami-yuk <e.y062097@gmail.com>
2020-11-13 15:53:33 +0900

    preview

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,12 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    const addProfit = document.getElementById("profit")
-    addProfit.innerHTML = Math.floor(inputValue * 0.9);
-  })
-});
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ ) || document.URL.match( /items/ )) {
+  window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+      const addProfit = document.getElementById("profit")
+      addProfit.innerHTML = Math.floor(inputValue * 0.9);
+    })
+  });
+}
